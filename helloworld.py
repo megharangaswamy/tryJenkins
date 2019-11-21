7bc45f18e17cca36d2cb6107f72ddb74577087c4
megha Rangaswamy <megha.rangaswamy@lindenbaum.eu>
2019-11-21 15:07:42 +0100

    next check in

--- helloworld.py
+++ helloworld.py
@@ -1,2 +1,3 @@
 print("helloworld")
 print("I am trying to add the github project to Jenkins and try building it.")
+print("adding new line to check if the commit will trigger the build in Jenkins.")
