209adc8d0fc9417c4cd3970772535204f962f7b0
megha Rangaswamy <megha.rangaswamy@lindenbaum.eu>
2019-11-21 15:28:25 +0100

    correcting the error

--- helloworld.py
+++ helloworld.py
@@ -1,3 +1,4 @@
+l = "hi"
 print("helloworld")
 print("I am trying to add the github project to Jenkins and try building it.")
 print("adding new line to check if the commit will trigger the build in Jenkins.")
